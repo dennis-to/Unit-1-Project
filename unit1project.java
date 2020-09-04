56d39525ebc615ee69b238c0a3a66b7b6f5c8046
Dennis To <dnnst3638@gmail.com>
2020-09-04 14:59:47 -0700

    add unit1project.java part 2

--- unit1project.java
+++ unit1project.java
@@ -14,12 +14,97 @@ public class unit1project{
     double finaltemp = reader.nextDouble();
     if(finaltemp < -273.14)
         finaltemp = -273.14;
+
+    String initialphase = "liquid";
+        if(initialtemp < 0)
+            initialphase = "solid";
+        if(initialtemp > 100)
+            initialphase = "vapor";
+    
+    String finalphase = "Liquid";
+        if(finaltemp < 0)
+            finalphase = "solid";
+        if(finaltemp > 100)
+            finalphase = "vapor";
     
     System.out.println("mass: " + mass + "g");
-    System.out.println("initial temeperature: " + initialtemp + "degrees celcius");
-    System.out.println("final temperature: " + finaltemp + "degrees celcius");
+    System.out.println("initial temeperature: " + initialtemp + " degrees celcius " + initialphase);
+    System.out.println("final temperature: " + finaltemp + " degrees celcius " + finalphase);
+
+    boolean endothermic = false;
+    if(finaltemp > initialtemp)
+        endothermic = true;
 
     }
+public static double tempchangesolid(double mass, double starttemp, double endtemp, String endphase, boolean endothermic){
+    if(!endphase.equals("solid"))
+        endtemp = 0;
+    double energychange = round(mass*0.002108*(endtemp - starttemp));
+    if(endothermic)
+        System.out.println("Heating (solid): " + energychange + " kj " );
+    else
+        System.out.println("Cooling (solid): " + energychange + " kj " );
+    return energychange;
+
+}
+
+public static double tempchangevapor(double mass, double starttemp, double endtemp, String endphase, boolean endothermic){
+    if(!endphase.equals("vapor"))
+        endtemp = 100;
+    double energychange = round(mass*0.001996*(endtemp - starttemp));
+    if(endothermic)
+        System.out.println("Heating (vapor): " + energychange + " kj " );
+    else
+        System.out.println("Cooling (vapor): " + energychange + " kj " );
+    return energychange;
+
+}
+public static double tempchangeliquid(double mass, double starttemp, double endtemp, String endphase, boolean endothermic){
+    if(endphase.equals("solid"))
+        endtemp = 0;
+    if(endphase.equals("vapor"))
+        endtemp = 100;
+    double energychange = round(mass*0.004184*(endtemp - starttemp));
+    if(endothermic)
+        System.out.println("Heating (solid): " + energychange + " kj " );
+    else
+        System.out.println("Cooling (solid): " + energychange + " kj " );
+    return energychange;
+
+}
+public static double fusion(double mass, boolean endothermic){
+    double energychange;
+    if(endothermic){
+        energychange = round(0.333*mass);
+        System.out.println("Melting: " + energychange + " kj " );
+    }
+    else {
+        energychange = round(-0.333*mass);
+        System.out.println("Freezing: " + energychange + " kj " );
+    }
+    return energychange;
+}
+public static double vaporization(double mass, boolean endothermic){
+    double energychange;
+    if(endothermic){
+        energychange = round(2.257*mass);
+        System.out.println("Evaporization: " + energychange + " kj " );
+    }
+    else {
+        energychange = round(-2.257*mass);
+        System.out.println("Condensation: " + energychange + " kj " );
+    }
+    return energychange;
+
+}
+public static double round(double x){
+    x *= 1000;
+    if(x > 0)
+        return (int)(x + 0.5)/1000.0;
+    else
+        return (int)(x - 0.5)/1000.0;
+}
+
 }
 
 
